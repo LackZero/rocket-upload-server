49a7d054b8ac70be5fff397d72563ddd9a5070b8
MrSeaWave <MrDaemon@outlook.com>
2022-01-10 15:53:35 +0800

    fix: 错误名字展示

--- src/middlewares/errors.js
+++ src/middlewares/errors.js
@@ -7,7 +7,7 @@ async function errorHandler(ctx, next) {
     // will only respond with JSON
     ctx.status = err.statusCode || err.status || 500;
     ctx.body = {
-      msg: err.message
+      msg: `${err.name}: ${err.message}`
     };
     // ctx.app.emit('error', err, ctx);
     // throw err;
